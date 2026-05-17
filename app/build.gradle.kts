827ab1cc812ce2b9e48e5fc65fa5b25129803126
zhkmxx9302013 <zhkmxx9302023@gmail.com>
2026-05-17 12:38:59 +0800

    Sign release APK for preview installs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
